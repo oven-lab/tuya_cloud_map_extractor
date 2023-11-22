40dbfe917940613ac77f3e3a048bb815dd40fedd
oven-lab <waldemar.bjornstrom@unfnorrbotten.se>
2023-11-22 19:49:43 +0000

    Part way to incorporating colors in config flow

--- custom_components/tuya_cloud_map_extractor/config_flow.py
+++ custom_components/tuya_cloud_map_extractor/config_flow.py
@@ -12,6 +12,7 @@ from .tuya_vacuum_map_extractor import (
 
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.selector import selector
 from homeassistant.const import (
     CONF_NAME,
     CONF_CLIENT_ID,
@@ -30,6 +31,12 @@ from .const import (
     CONF_SERVER_CENTRAL_EUROPE,
     CONF_SERVER_WEST_EUROPE,
     CONF_SERVER_INDIA,
+    CONF_COLORS,
+    CONF_BG_COLOR,
+    CONF_WALL_COLOR,
+    CONF_ROOM_COLORS,
+    CONF_ROOM_COLOR,
+    CONF_ROOM_NAME,
 )
 
 CONF_SERVERS = {
@@ -38,13 +45,24 @@ CONF_SERVERS = {
     CONF_SERVER_EAST_AMERICA: "Eastern America",
     CONF_SERVER_CENTRAL_EUROPE: "Central Europe",
     CONF_SERVER_WEST_EUROPE: "Western Europe",
-    CONF_SERVER_INDIA: "India",
+    CONF_SERVER_INDIA: "India"
 }
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    def __init__(self) -> None:
+        super().__init__()
+        self.map_header = {}
+        self._name = ""
+        self._server = ""
+        self._client_id = ""
+        self._client_secret = ""
+        self._device_id = ""
+        self._bg_color = None
+        self._wall_color = None
+
     async def async_step_user(self, user_input=None):
         default_server = CONF_SERVER_CENTRAL_EUROPE
         default_name = "Vacuum map"
@@ -56,9 +74,16 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             try:
                 headers, image = await validate(self.hass, user_input)
-                return self.async_create_entry(
-                    title=user_input.pop(CONF_NAME), data=user_input
-                )
+                self.map_header = headers
+                self._name = user_input["name"]
+                self._server = user_input["server"]
+                self._client_id = user_input["client_id"]
+                self._client_secret = user_input["client_secret"]
+                self._device_id = user_input["device_id"]
+                if user_input[CONF_COLORS]:
+                    return await self.async_step_colorconf()
+                else:
+                    print("add to registry")
             except ClientIDError:
                 errors[CONF_CLIENT_ID] = "client_id"
             except ClientSecretError:
@@ -83,10 +108,64 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             vol.Required(CONF_CLIENT_ID, default=default_client_id): str,
             vol.Required(CONF_CLIENT_SECRET, default=default_client_secret): str,
             vol.Required(CONF_DEVICE_ID, default=default_device_id): str,
+            vol.Required(CONF_COLORS, default=False): bool,
         }
 
         return self.async_show_form(
             step_id="user", data_schema=vol.Schema(DATA_SCHEMA), errors=errors
+        )
+
+    async def async_step_colorconf(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            if not CONF_BG_COLOR in user_input:
+                user_input[CONF_BG_COLOR] = [0, 0, 0]
+            if not CONF_WALL_COLOR in user_input:
+                user_input[CONF_WALL_COLOR] = [0, 0, 0]
+
+            if user_input[CONF_ROOM_COLORS]:
+                self._bg_color = user_input[CONF_BG_COLOR]
+                self._wall_color = user_input[CONF_WALL_COLOR]
+                return await self.async_step_room_colors()
+            else:
+                print("add to registry")
+
+        DATA_SCHEMA = {
+            CONF_BG_COLOR: selector(
+                {"color_rgb": {}}  # TODO: default value of [44, 50, 64]
+            ),
+            CONF_WALL_COLOR: selector(
+                {"color_rgb": {}}  # TODO: default value of [255, 255, 255]
+            ),
+        }
+
+        if "roominfo" in self.map_header:
+            DATA_SCHEMA[vol.Required(CONF_ROOM_COLORS, default=False)] = bool
+
+        return self.async_show_form(
+            step_id="colorconf", data_schema=vol.Schema(DATA_SCHEMA), errors=errors
+        )
+
+    async def async_step_room_colors(self, user_input=None):
+        errors = {}
+        rooms = self.map_header["roominfo"]
+        DATA_SCHEMA = {}
+
+        if user_input is not None:
+            for i in rooms:
+                if not (CONF_ROOM_COLOR + str(i["ID"])) in user_input:
+                    user_input[CONF_ROOM_COLOR + str(i["ID"])] = [0, 0, 0]
+
+            print("Add to registry: ", user_input)
+
+        for i in rooms:
+            DATA_SCHEMA[
+                vol.Required(CONF_ROOM_NAME + str(i["ID"]), default=i["name"])
+            ] = str
+            DATA_SCHEMA[CONF_ROOM_COLOR + str(i["ID"])] = selector({"color_rgb": {}})
+
+        return self.async_show_form(
+            step_id="room_colors", data_schema=vol.Schema(DATA_SCHEMA), errors=errors
         )
 
 
--- custom_components/tuya_cloud_map_extractor/const.py
+++ custom_components/tuya_cloud_map_extractor/const.py
@@ -4,7 +4,6 @@ PLATFORMS = ["camera"]
 CONTENT_TYPE = "image/png"
 DEFAULT_NAME = "Tuya Cloud Map Extractor"
 
-
 CONF_SERVER = "server"
 CONF_SERVER_CHINA = "https://openapi.tuyacn.com"
 CONF_SERVER_WEST_AMERICA = "https://openapi.tuyaus.com"
@@ -12,3 +11,10 @@ CONF_SERVER_EAST_AMERICA = "https://openapi-ueaz.tuyaus.com"
 CONF_SERVER_CENTRAL_EUROPE = "https://openapi.tuyaeu.com"
 CONF_SERVER_WEST_EUROPE = "https://openapi-weaz.tuyaeu.com"
 CONF_SERVER_INDIA = "https://openapi.tuyain.com"
+
+CONF_COLORS = "color_conf"
+CONF_BG_COLOR = "bg_color"
+CONF_WALL_COLOR = "wall_color"
+CONF_ROOM_COLORS = "room_colors"
+CONF_ROOM_COLOR = "room_color_"
+CONF_ROOM_NAME = "room_name_"
